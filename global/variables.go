627c53e53b4a38743b54383fb6a97ed4e4c9ca8c
Heier <alexander@engebrigtsenheier.no>
2024-06-16 14:25:56 +0200

    ADD: Readme.md

--- global/variables.go
+++ global/variables.go
@@ -1,16 +1,24 @@
 package global
 
-// can be changed
+/**
+VAR THAT CAN BE CHANGED
+*/
 
+// If the video is to be posted to Youtube or not.
 var PostVideo bool = false
 
+// Flag for Youtube if the video is made for kids or not
 var MadeForKids bool = false
 
+// The speed of the AI voice
 var VoiceSpeed float64 = 1
 
+// if the video is to be deleted when done
 var DeleteVideoAfterPost bool = false
 
-var Themas []string = []string{ // max 15 entries
+// max 15 entries
+// make sure to test the new thema to make sure there exsist a quote and video for said thema.
+var Themas []string = []string{
 	"love",
 	"friendship",
 	"happiness",
@@ -20,6 +28,9 @@ var Themas []string = []string{ // max 15 entries
 	"trust",
 }
 
-// Do not change
+/**
+DO NOT CHANGE
+*/
 
+// The Youtube # limit
 const TagLimit int = 15
