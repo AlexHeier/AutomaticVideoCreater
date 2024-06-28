736d8e9932f8a0ddc130dba1081971630f685437
Heier <alexander@engebrigtsenheier.no>
2024-06-28 21:47:44 +0200

    FIX: permission issue

--- gitPull.sh
+++ gitPull.sh
@@ -4,14 +4,14 @@ GIT_REPO_DIR="/home/heier/videoCreater"
 
 cd "$GIT_REPO_DIR" || exit 1
 
-
+current_head=$(git rev-parse HEAD)
 git fetch origin main
+git reset --hard FETCH_HEAD
+new_head=$(git rev-parse HEAD)
 
-
-git checkout FETCH_HEAD -- .
-
-
-if ! git diff --quiet; then
+if [ "$current_head" != "$new_head" ]; then
     go build -o videomaker main.go
     chmod +x videomaker
 fi
+
+chmod +x gitPull.sh
