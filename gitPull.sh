2eac45c5fdb3cb950839049d20d33141e2c95494
Heier <alexander@engebrigtsenheier.no>
2024-06-29 15:10:21 +0200

    FIX?: Format of go.mod

--- gitPull.sh
+++ gitPull.sh
@@ -10,12 +10,8 @@ git reset --hard FETCH_HEAD
 new_head=$(git rev-parse HEAD)
 
 if [ "$current_head" != "$new_head" ]; then
-    # Retrieve the Go version installed on the server
-    go_version=$(go version | awk '{print $3}')
-    major_minor_version=$(echo "$go_version" | grep -oP 'go\K[0-9]+\.[0-9]+')
-
-    # Update the go.mod file to use the server's Go version
-    sed -i "s/^go [0-9]\+\.[0-9]\+$/go ${major_minor_version}/" go.mod
+    # Update the go.mod file to ensure the Go version is in the format 1.x
+    sed -i -E 's/^go ([0-9]+\.[0-9]+)\.[0-9]+$/go \1/' go.mod
 
     go build -o videomaker main.go
     chmod +x videomaker
